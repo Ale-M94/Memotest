18aed123a9f2fb6fd0de8d4dc0ed5b71d60fd137
Ale-M94 <175752593+Ale-M94@users.noreply.github.com>
2024-11-14 17:23:11 -0300

    Agrega las pruebas del juego
    Una vez configurado el tablero prueba las combinaciones de colores distintos y juego completo, incluyendo prueba de mensaje final

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -5,10 +5,9 @@ context('Memotest', () => {
   beforeEach(() => {
     cy.visit(URL);
   });
+  const NUM_CUADROS = 12;
 
   describe('configura el juego', () => {
-    const NUM_CUADROS = 12;
-
     it('comprueba la cantidad de cuadros', () => {
       cy.get('#tablero').find('.cuadro').should('have.length', NUM_CUADROS);
     });
@@ -34,5 +33,59 @@ context('Memotest', () => {
 
     });
   });
+
+  describe('prueba el juego', () => {
+    let mapaPares;
+    let listaPares;
+
+    it('prueba haciendo click en dos cuadros de color distinto', () => {
+      cy.get('.cuadro').then(cuadros => {
+        mapaPares = obtenerPares(cuadros);
+        listaPares = Object.values(mapaPares);
+
+        cy.get(listaPares[0][0]).click();
+        cy.get(listaPares[1][0]).click();
+
+        cy.get('.cuadro').should('have.length', NUM_CUADROS);
+      });
+    });
+
+    it('completa el juego', () => {
+      cy.get('.cuadro').then(cuadros => {
+        mapaPares = obtenerPares(cuadros);
+        listaPares = Object.values(mapaPares);
+
+        listaPares.forEach((par)=>{
+          cy.get(par[0]).click();
+          cy.get(par[1]).click();
+        });
+
+        cy.get('.cuadro').should('have.length', 0);
+        cy.get('#tablero').should('not.be.visible');
+        cy.get('#mensaje-fin').should('be.visible');
+
+        const NUM_TURNOS = 6;
+        cy.get('#turnos').contains(`${NUM_TURNOS}`);
+      });
+    });
+
+  });
 });
+
+
+  function obtenerPares(cuadros) {
+    let pares = {};
+
+    cuadros.each(function (i, cuadro) {
+      const claseCuadro = cuadro.className.replace('cuadro h-100 ', '');
+
+      if (pares[claseCuadro]) {
+        pares[claseCuadro].push(cuadro);
+      } else {
+        pares[claseCuadro] = [cuadro];
+      };
+    });
+
+    return pares;
+  };
 
